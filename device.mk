25d74f1fbfabd5bee24728f7396dee272baeb7bf
BambooIV <ningdisheng@hotmail.com>
2019-09-21 14:23:24 +0800

    nx611j: Add privapp-permissions-qti.xml

--- device.mk
+++ device.mk
@@ -320,6 +320,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# QCOM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
